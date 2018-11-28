638826f457c50581a97c578615d9b616f33b0c02
Miguel Rodrigues <44868441+miguel-135@users.noreply.github.com>
2018-11-28 18:29:27 +0000

    Update Task2.m

--- Parte2/Task2.m
+++ Parte2/Task2.m
@@ -1,9 +1,9 @@
 %Correr
 x0 = [-1 ; -1 ; 0];
 e=10^(-6);
-data = 'data1';
+data = 'data2';
 load(data);
-x=GradientAlgorithm(x0,e,data)
+[x,g]=GradientAlgorithm(x0,e,data)
 
 %Plot
 figure(1)
@@ -18,6 +18,11 @@ end
 ponto1 = [6 , ( x(3) - x(2)*6 ) / ( x(1) )];
 ponto2 = [( x(3) - x(1)*6 ) / ( x(2) ) , 6];
 plot( ponto1 , ponto2 , 'g--','LineWidth',1.5)
+figure(2)
+for i=1:length(g)
+norm_g(i)=log10(norm( g(1:3,i) ));
+end
+plot( 1:1:length(g) , norm_g )
     
     
     
