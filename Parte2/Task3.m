24367551278c62991eddcb1ef67dd963285e4d38
Miguel Rodrigues <44868441+miguel-135@users.noreply.github.com>
2018-11-28 20:48:32 +0000

    Create Task3.m

---
+++ Parte2/Task3.m
@@ -0,0 +1,27 @@
+%Correr
+x0 = [-1 ; -1 ; 0];
+e=10^(-6);
+data = 'data2';
+load(data);
+[x,g]=GradientAlgorithm(x0,e,data);
+norm_g=0;
+
+%Plot
+figure(1)
+hold on
+for i=1:150
+    if Y(i)==1
+        plot(X(1,i),X(2,i),'bo','LineWidth',1.5);
+    else
+        plot(X(1,i),X(2,i),'ro','LineWidth',1.5);
+    end
+end
+ponto1 = [5.5 , ( x(3) - x(2)*3.2 ) / ( x(1) )];
+ponto2 = [( x(3) - x(1)*5.5 ) / ( x(2) ) , 3.2];
+plot( ponto1 , ponto2 , 'g--','LineWidth',1.5)
+figure(2)
+for i=1:length(g)
+norm_g(i)=norm( g(1:3,i) );
+end
+semilogy( 1:1:length(g) , norm_g ,'LineWidth',1.5);
+grid on
