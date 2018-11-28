ae3c6ff0d74fcc2ddfe85af2c5f88416f8a7a493
Miguel Rodrigues <44868441+miguel-135@users.noreply.github.com>
2018-11-28 20:48:28 +0000

    Update GradientAlgorithm.m

--- Parte2/GradientAlgorithm.m
+++ Parte2/GradientAlgorithm.m
@@ -1,13 +1,13 @@
 function [x,g]=GradientAlgorithm(x0, e, data)   
-    g(1:3,1)=GradF(x0,data)
+    g(1:length(x0),1)=GradF(x0,data);
     x=x0;
     k=1;
     a=1;
-    while(norm( g(1:3,k) ) > e)  
-       d=-g(1:3,k);
-       a=Backtracking(x,d,data)
+    while(norm( g(1:length(x0),k) ) > e)  
+       d=-g(1:length(x0),k);
+       a=Backtracking(x,d,data);
        x=x+d*a;
        k=k+1;
-       g(1:3,k)=GradF(x,data);
+       g(1:length(x0),k)=GradF(x,data);
     end
 end
