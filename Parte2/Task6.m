04c1a764631bc50c15c57059651ad9f11333c97f
Miguel Rodrigues <44868441+miguel-135@users.noreply.github.com>
2018-12-02 19:20:39 +0000

    Update Task6.m

--- Parte2/Task6.m
+++ Parte2/Task6.m
@@ -1,28 +1,58 @@
-%Correr
+%Correr data1.m
 x0 = [-1 ; -1 ; 0];
 e=10^(-6);
 data = 'data1';
 load(data);
-[x,g]=NewtonAlgorithm(x0,e,data);
+[x,g,a]=NewtonAlgorithm(x0,e,data);
 norm_g=0;
 
-%Plot
+%Plot data1.m
 figure(1)
-hold on
-for i=1:150
-    if Y(i)==1
-        plot(X(1,i),X(2,i),'bo','LineWidth',1.5);
-    else
-        plot(X(1,i),X(2,i),'ro','LineWidth',1.5);
-    end
-end
-ponto1 = [6 , ( x(3) - x(2)*6 ) / ( x(1) )];
-ponto2 = [( x(3) - x(1)*6 ) / ( x(2) ) , 6];
-plot( ponto1 , ponto2 , 'g--','LineWidth',1.5)
-figure(2)
 for i=1:length(g)
 norm_g(i)=norm( g(1:3,i) );
 end
-
 semilogy( 1:1:length(g) , norm_g ,'LineWidth',1.5);
 grid on
+
+figure(2)
+stem(1:1:length(a),a(:),'filled','Linewidth',2);
+
+%%
+%Correr data2.m
+x0 = [-1 ; -1 ; 0];
+e=10^(-6);
+data = 'data2';
+load(data);
+[x,g,a]=NewtonAlgorithm(x0,e,data);
+norm_g=0;
+
+%Plot data2.m
+figure(3)
+for i=1:length(g)
+norm_g(i)=norm( g(1:3,i) );
+end
+semilogy( 1:1:length(g) , norm_g ,'LineWidth',1.5);
+grid on
+
+figure(4)
+stem(1:1:length(a),a(:),'filled','Linewidth',2);
+
+%%
+%Correr data3.m
+x0 = [ -1*ones(1,30) , 0 ]';
+e=10^(-6);
+data = 'data3';
+load(data);
+[x,g,a]=NewtonAlgorithm(x0,e,data);
+norm_g=0;
+
+%Plot data3.m
+figure(5)
+for i=1:12
+norm_g(i)=norm( g(1:31,i) );
+end
+semilogy( 1:12 , norm_g ,'LineWidth',1.5);
+grid on
+
+figure(6)
+stem(1:1:length(a),a(:),'filled','Linewidth',2);
