b1a1ddb01446ac2ff15928b340392b5a96cf3e8d
Miguel Rodrigues <44868441+miguel-135@users.noreply.github.com>
2018-12-02 19:20:31 +0000

    Update NewtonAlgorithm.m

--- Parte2/NewtonAlgorithm.m
+++ Parte2/NewtonAlgorithm.m
@@ -1,19 +1,19 @@
-function [x,g]=NewtonAlgorithm(x0, e, data)   
+function [x,g,a]=NewtonAlgorithm(x0, e, data)   
     g(1:length(x0),1)=GradF(x0,data);
     g2(1:length(x0),1:length(x0))=GradF2(x0,data);
     x=x0;
     k=1;
     a=1;
-    aux=sym('d',[3,1]);
+    aux=sym('d',[length(x0),1]);
     while(norm( g(1:length(x0),k) ) > e) 
        % Resolve o sistema linear para determinar o dk, e converte o resultado do solve em doubles;
        eqn = [ g2*aux==-g(1:length(x0),k) ];
        sol = solve( eqn , aux );
        AUX = struct2cell(sol);
-       d = double(cat(1,AUX{:}))
+       d = double(cat(1,AUX{:}));
        % Aplica o Backtracking e atualiza as variaveis necessárias 
-       a=Backtracking(x,d,data)
-       x=x+d*a;
+       a(k)=Backtracking(x,d,data);
+       x=x+d*a(k);
        k=k+1;
        g(1:length(x0),k)=GradF(x,data);
        g2(1:length(x0),1:length(x0))=GradF2(x,data);
