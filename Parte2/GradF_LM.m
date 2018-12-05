e1a07e07104853dcaaf5eeb4090f8bee5c2bef59
Miguel Rodrigues <44868441+miguel-135@users.noreply.github.com>
2018-12-05 23:43:53 +0000

    Update GradF_LM.m

--- Parte2/GradF_LM.m
+++ Parte2/GradF_LM.m
@@ -1,5 +1,4 @@
-function g_lm=GradF_LM(x, data)
-    
+function [g_lm,g_lm_j]=GradF_LM(x, data)
      load(data);
      %Cria a matriz A de forma a -sp=A*x
      for k1=1:length(iA)
@@ -31,19 +30,27 @@ function g_lm=GradF_LM(x, data)
      end
      
      %Calculo do gradiente de f1
-     aux=0;
+     aux_1 = 0;
+     aux_2 = 0;
      for j=1:length(y)
-         aux = aux + ( (y(j)/sqrt( ( A_s(j*2-1,:)*x+b_s(j*2-1) )^2 + ( A_s(j*2,:)*x+b_s(j*2) )^2 )) * ( A_s(j*2-1:j*2,:)'*A_s(j*2-1:j*2,:)*x + A_s(j*2-1:j*2,:)'*b_s(j*2-1:j*2) ) );
+         aux_1 = ( (y(j)/sqrt( ( A_s(j*2-1,:)*x+b_s(j*2-1) )^2 + ( A_s(j*2,:)*x+b_s(j*2) )^2 )) * ( A_s(j*2-1:j*2,:)'*A_s(j*2-1:j*2,:)*x + A_s(j*2-1:j*2,:)'*b_s(j*2-1:j*2) ) );
+         aux_2 = aux_2 + aux_1;
+         grad_f1_j(j,:) = 2*A_s(j*2-1:2*j,:)'*A_s(j*2-1:2*j,:)*x + 2*A_s(j*2-1:2*j,:)'*b_s(j*2-1:j*2) - 2*aux_1;
      end
-     grad_f1 = 2*A_s'*A_s*x + 2*A_s'*b_s - 2*aux;
+     grad_f1 = 2*A_s'*A_s*x + 2*A_s'*b_s - 2*aux_2;
      
      %Calculo do gradiente de f2
-     aux=0;
+     aux_1 = 0;
+     aux_2 = 0;
      for j=1:length(z)
-         aux = aux + ( (z(j)/sqrt( ( B_s(j*2-1,:)*x )^2 + ( B_s(j*2,:)*x )^2 )) * ( B_s(j*2-1:j*2,:)'*B_s(j*2-1:j*2,:)*x ) );
+         aux_1 = ( (z(j)/sqrt( ( B_s(j*2-1,:)*x )^2 + ( B_s(j*2,:)*x )^2 )) * ( B_s(j*2-1:j*2,:)'*B_s(j*2-1:j*2,:)*x ) );
+         aux_2 = aux_2 + aux_1;
+         grad_f2_j(j,:) = 2*B_s(j*2-1:2*j,:)'*B_s(j*2-1:2*j,:)*x - 2*aux_1;
      end
-     grad_f2 = 2*B_s'*B_s*x - 2*aux;
+     grad_f2 = 2*B_s'*B_s*x - 2*aux_2;
      
      %Calculo do gradiente de f
      g_lm = grad_f1 + grad_f2;
+     %Criação do vetor g_lm_j = ¨[ f1_j f2_j ]'
+     g_lm_j = [ grad_f1_j ; grad_f2_j ];
 end
