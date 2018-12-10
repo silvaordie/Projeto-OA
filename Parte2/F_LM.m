9aede0073c1ac5004c02514bd5a7d9e8fc51ea6a
Miguel Rodrigues <44868441+miguel-135@users.noreply.github.com>
2018-12-10 18:17:02 +0000

    Update F_LM.m

--- Parte2/F_LM.m
+++ Parte2/F_LM.m
@@ -35,7 +35,8 @@ function [f_lm,f_lm_j]=F_LM(x, data)
      for j=1:length(y)
          aux_1 = y(j)*sqrt( ( A_s(j*2-1,:)*x+b_s(j*2-1) )^2 + ( A_s(j*2,:)*x+b_s(j*2) )^2 );
          aux_2 = aux_2 + aux_1;
-         f1_j(j,1) = x'*A_s(j*2-1:2*j,:)'*A_s(j*2-1:2*j,:)*x + b_s(j*2-1:j*2)'*b_s(j*2-1:j*2) + y(j)^2 + 2*b_s(j*2-1:j*2)'*A_s(j*2-1:2*j,:)*x - 2*aux_1;
+         %f1_j(j,1) = x'*A_s(j*2-1:2*j,:)'*A_s(j*2-1:2*j,:)*x + b_s(j*2-1:j*2)'*b_s(j*2-1:j*2) + y(j)^2 + 2*b_s(j*2-1:j*2)'*A_s(j*2-1:2*j,:)*x - 2*aux_1;
+         f1_j(j,1) = sqrt( ( A_s(j*2-1,:)*x+b_s(j*2-1) )^2 + ( A_s(j*2,:)*x+b_s(j*2) )^2 ) - y(j);
      end
      som_f1 = x'*A_s'*A_s*x + b_s'*b_s + y'*y + 2*b_s'*A_s*x - 2*aux_2;
      
@@ -45,7 +46,8 @@ function [f_lm,f_lm_j]=F_LM(x, data)
      for j=1:length(z)
         aux_1 = z(j)*sqrt( (B_s(j*2-1,:)*x)^2 + (B_s(j*2,:)*x)^2 );
         aux_2 = aux_2 + aux_1;
-        f2_j(j,1) = x'*B_s(j*2-1:2*j,:)'*B_s(j*2-1:2*j,:)*x + z(j)^2 - 2*aux_1;
+        %f2_j(j,1) = x'*B_s(j*2-1:2*j,:)'*B_s(j*2-1:2*j,:)*x + z(j)^2 - 2*aux_1;
+        f2_j(j,1) = sqrt( (B_s(j*2-1,:)*x)^2 + (B_s(j*2,:)*x)^2 ) -z(j);
      end
      som_f2 = x'*B_s'*B_s*x + z'*z - 2*aux_2;
      
